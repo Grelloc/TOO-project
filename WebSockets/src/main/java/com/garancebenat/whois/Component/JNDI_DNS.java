8296c4bbb952fb709cda7be86cabadf4fe46666f
Grelloc <benat.moi@gmail.com>
2020-10-21 21:24:02 +0200

    Trying to fix the .idea pb

--- WebSockets/src/main/java/com/garancebenat/whois/Component/JNDI_DNS.java
+++ WebSockets/src/main/java/com/garancebenat/whois/Component/JNDI_DNS.java
@@ -1,19 +1,23 @@
 package com.garancebenat.whois.Component;
 
+import com.sun.jndi.dns.DnsContext;
+import lombok.experimental.UtilityClass;
+
 import javax.naming.*;
 import javax.naming.directory.*;
 
+@UtilityClass
 public class JNDI_DNS {
 
-    public JNDI_DNS() {
+    public void codeBarbier() {
         try {
             java.util.Properties _p = new java.util.Properties();
             // For Java 9 and more: https://mvnrepository.com/artifact/com.sun.jndi/dns:
             _p.setProperty(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.dns.DnsContextFactory");
             // Use another DNS server (platform config. otherwise: https://docs.oracle.com/javase/7/docs/technotes/guides/jndi/jndi-dns.html#URL):
-            // _p.setProperty(Context.PROVIDER_URL, "dns://194.167.156.13"); // UPPA
+//             _p.setProperty(Context.PROVIDER_URL, "dns://194.167.156.13"); // UPPA
             _p.setProperty(Context.PROVIDER_URL, "dns://8.8.8.8"); // Google
-            // _p.setProperty(Context.PROVIDER_URL, "dns://193.146.78.14"); // University of Mondragon
+//             _p.setProperty(Context.PROVIDER_URL, "dns://193.146.78.14"); // University of Mondragon
             DirContext dc = new InitialDirContext(_p);
             Attributes attributes = dc.getAttributes("FranckBarbier.com", new String[]{"NS"}); // Stores the name server for a DNS entry...
             if (attributes != null) {
@@ -24,11 +28,11 @@ public class JNDI_DNS {
             }
 // Get all Web address suffixes:
             InitialContext _ic = new InitialContext(_p);
-            System.out.println("\nInitial context: " + _ic.getNameInNamespace());
+//            System.out.println("\nInitial context: " + _ic.getNameInNamespace());
             NamingEnumeration ne = _ic.list("");
-            while (ne.hasMore()) {
-                System.out.println("\t" + ((NameClassPair) (ne.next())).getName());
-            }
+//            while (ne.hasMore()) {
+//                System.out.println("\t" + ((NameClassPair) (ne.next())).getName());
+//            }
 
             NameParser np = _ic.getNameParser(_ic.getNameInNamespace());
             Name university_of_Mondragon = np.parse("www.mondragon.edu");
@@ -36,8 +40,8 @@ public class JNDI_DNS {
             for (int i = 0; i < university_of_Mondragon.size(); i++) {
                 System.out.print("\t" + university_of_Mondragon.get(i));
             }
-            Object o = _ic.lookup(university_of_Mondragon.getPrefix(university_of_Mondragon.size() - 1));
-            ne = (((com.sun.jndi.dns.DnsContext) o).getAttributes(university_of_Mondragon, null)).getAll();
+            DnsContext o = (DnsContext) _ic.lookup(university_of_Mondragon.getPrefix(university_of_Mondragon.size() - 1));
+            ne = ((o).getAttributes(university_of_Mondragon, null)).getAll();
             while (ne.hasMore()) {
                 BasicAttribute ba = (BasicAttribute) ne.next();
                 System.out.print("\n\tAttribute id. [" + ba.getID() + "]: ");
--- WebSockets/src/main/java/com/garancebenat/whois/WhoIs.java
+++ WebSockets/src/main/java/com/garancebenat/whois/WhoIs.java
@@ -4,6 +4,7 @@ import com.garancebenat.whois.Component.JNDI_DNS;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import java.io.IOException;
 import java.net.URI;
@@ -13,15 +14,18 @@ import static java.awt.Desktop.getDesktop;
 @Slf4j
 @SpringBootApplication
 public class WhoIs {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try {
             getDesktop()
                     .browse(URI.create("http://localhost:1963/WhoIs"));
         } catch (IOException e) {
             log.error("Gars relou{}", "Bonjour","Comment va?", e); //Permet de mettre le bonjour dans les {}
         }
-        SpringApplication.run(WhoIs.class, args);
-        new JNDI_DNS();
-//        SpringApplication.exit();
+        final ConfigurableApplicationContext run = SpringApplication.run(WhoIs.class, args);
+//        new JNDI_DNS();
+        java.io.BufferedReader reader = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
+        System.out.println("Please press a key to stop the server...");
+        reader.readLine();
+        SpringApplication.exit(run);
     }
 }
