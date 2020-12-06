ac59850ffda2d4c188eb2e833bdf4af60aa2cc25
Phanta8 <garance.duci@gmail.com>
2020-12-06 14:57:56 +0100

    js, html, css update

--- WebSockets/src/main/resources/static/index.js
+++ WebSockets/src/main/resources/static/index.js
@@ -27,7 +27,39 @@ function FormatURL(){
 }
 
 function printRecord(record){
-    
+  console.log(record);
+  // get the reference for the body
+  var div = document.getElementById("tableau");
+ 
+  // creates a <table> element and a <tbody> element
+  var tbl = document.createElement("table");
+  var tblBody = document.createElement("tbody");
+ 
+  // creating all cells
+  for (var i = 0; i < record.length; i++) {
+    // creates a table row
+    var row = document.createElement("tr");
+ 
+    for (var j = 0; j < record[i].length; j++) {
+      // Create a <td> element and a text node, make the text
+      // node the contents of the <td>, and put the <td> at
+      // the end of the table row
+      var cell = document.createElement("td");
+      var cellText = document.createTextNode("cell in row "+i+", column "+j);
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    }
+ 
+    // add the row to the end of the table body
+    tblBody.appendChild(row);
+  }
+ 
+  // put the <tbody> in the <table>
+  tbl.appendChild(tblBody);
+  // appends <table> into <div>
+  div.appendChild(tbl);
+  // sets the border attribute of tbl to 2;
+  tbl.setAttribute("border", "2");
 }
 
 function getURL(){
@@ -43,19 +75,10 @@ function getURL(){
             else{
                 throw new Error('Something went wrong on api server');
             }
-<<<<<<< HEAD
         }).catch(error => {
             console.error(error);
         });
-        printRecord(response);
-=======
-        }).then(response => {
-            console.debug(response);
-        }).catch(error => {
-            console.error(error);
-        });
-        request.parse;      
->>>>>>> 2bbe550967e835648b358f11347c1cfda1b3b107
+        printRecord(request.json);
     }else{
         alert("Wrong URL, please try again...");
     }
